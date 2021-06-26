38004433308aa3e018e169d3823d8ae40177f3b6
dudakvo <dudakvo@gmail.com>
2021-06-26 04:40:29 +0300

    Инициализация проекта. Первый комит

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+
+const {HttpCode}=require('./helpers/constants');
+
+const app = express();
+
+app.use(logger('combined'));
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res) => {
+    res.status(HttpCode.NOT_FOUND).json({ message: "Not found" });
+  });
+  
+  app.use((err, req, res, next) => {
+    const code = err.status || HttpCode.INTERNAL_SERVER_ERROR;
+    const status = err.status ? "error" : "fail";
+    res.status(code).json({ status, code, message: err });
+  });
+
+module.exports = app
